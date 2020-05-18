4c5c5170b6b4c85dc42d32bb653008ace22261b8
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-18 20:17:55 +0530

    Create PrefixSum_GetSum.cpp

---
+++ Arrays/7.PrefixSum_GetSum.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void display(vector<int> arr)
+{
+    for (auto i = arr.begin(); i != arr.end(); i++)
+        cout << *i << " ";
+    cout << endl;
+}
+
+// Time O(n)
+int sum_ab(vector<int> &arr, int x, int y)
+{
+    int n = arr.size();
+    int sum = 0;
+    for (int i = x; i <= y; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+vector<int> prefix_Sum(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> prefixSum(n);
+    prefixSum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
+    return prefixSum;
+}
+
+// prefixSum method | Time O(1)
+int getSum(vector<int> &arr, int x, int y)
+{
+    int sum;
+    vector<int> prefixSum = prefix_Sum(arr);
+
+    if (x == 0)
+        sum = prefixSum[y];
+    else
+        sum = prefixSum[y] - prefixSum[x - 1];
+
+    return sum;
+}
+
+int main()
+{
+    int a, b;
+    vector<int> A = {2, 8, 3, 9, 6, 5, 4};
+    cout << "Enter the two indices : ";
+    cin >> a >> b;
+    //cout << "Sum(" << a << ", " << b << ") = " << sum_ab(A, a, b) << endl;
+    cout << "getSum(" << a << ", " << b << ") = " << getSum(A, a, b);
+}

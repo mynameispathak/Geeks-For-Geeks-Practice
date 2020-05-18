5122c632d58f16a052a602ee1ad4065a46e56f95
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-18 23:53:09 +0530

    Create EqbmPt_Optimized.cpp

---
+++ Arrays/9.EqbmPt_Optimized.cpp
@@ -0,0 +1,30 @@
+// Equilibrium Point : Sum of Elements before it = Sum of Elements After it.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// prefixSum method | Time O(1) if prefixSum array is computed beforehand.
+string eqbmPt(vector<int> &arr)
+{
+    int sumL = 0, sum = 0, n = arr.size();
+    string str;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (sumL == sum - arr[i])
+            return "Yes";
+        sumL += arr[i];
+        sum -= arr[i];
+    }
+    return "No";
+}
+
+int main()
+{
+    int a, b;
+    vector<int> A = {3, 4, 8, -9, 20, 6};
+    cout << "Equilibrium Point Exists? : " << eqbmPt(A) << endl;
+}

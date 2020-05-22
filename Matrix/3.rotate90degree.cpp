df852c61e18a2b4b1d15e243709534f6ab3d1397
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-23 00:11:53 +0530

    Create rotate90degree.cpp

---
+++ Matrix/3.rotate90degree.cpp
@@ -0,0 +1,79 @@
+// Submitted by Aniket Pathak
+// mynameispathak
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Inplace Transpose of Matrix Time O(n2) | Space O(1)
+void transpose(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < arr[i].size(); j++)
+            swap(arr[i][j], arr[j][i]);
+    }
+}
+
+void rotate90CCW(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        int low = 0, high = n - 1;
+        while (low < high)
+        {
+            swap(arr[low][i], arr[high][i]);
+            low++;
+            high--;
+        }
+    }
+}
+
+void rotate90CW(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        int low = 0, high = n - 1;
+        while (low < high)
+        {
+            swap(arr[i][low], arr[i][high]);
+            low++;
+            high--;
+        }
+    }
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N);
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                int temp;
+                cin >> temp;
+                mat[i].push_back(temp);
+            }
+        }
+        transpose(mat);
+        rotate90CCW(mat);
+        //rotate90CW(mat);
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                cout << mat[i][j] << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}

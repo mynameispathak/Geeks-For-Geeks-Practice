eefaf392bdb9812a6999a157ee91324f40ebb8c3
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-05-22 22:07:42 +0530

    Create Transpose.cpp

---
+++ Matrix/2.Transpose.cpp
@@ -0,0 +1,43 @@
+// Submitted by Aniket Pathak
+// mynameispathak
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Inplace Transpose of Matrix Time O(n2) | Space O(1)
+vector<vector<int>> transpose(vector<vector<int>> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr[i].size(); j++)
+            swap(arr[i][j], arr[j][i]);
+    }
+    return arr;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N);
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                int temp;
+                cin >> temp;
+                mat[i].push_back(temp);
+            }
+        }
+        transpose(mat);
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                cout << mat[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}

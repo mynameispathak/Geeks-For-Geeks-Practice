8d3a22a0a3ad03a568a5e618966864af18422a51
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-06-04 12:57:51 +0530

    Create numOccurancesOptimized.cpp

---
+++ Searching/2.1numOccurancesOptimized.cpp
@@ -0,0 +1,55 @@
+// Submitted by Aniket Pathak
+// mynameispathak
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int first(vector<int> &arr, int l, int r, int x)
+{
+    if (l > r)
+        return -1;
+    int mid = (l + r) / 2;
+    if (arr[mid] == x && (arr[0] == x || arr[mid - 1] != x))
+        return mid;
+    else if (x > arr[mid])
+        first(arr, mid + 1, r, x);
+    else
+        first(arr, l, mid - 1, x);
+}
+
+int last(vector<int> &arr, int l, int r, int x)
+{
+    if (l > r)
+        return -1;
+    int mid = (l + r) / 2;
+    if (arr[mid] == x && (arr[arr.size() - 1] == x || arr[mid + 1] != x))
+        return mid;
+    else if (x >= arr[mid])
+        last(arr, mid + 1, r, x);
+    else
+        last(arr, l, mid - 1, x);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, X;
+        cin >> N >> X;
+        vector<int> A(N);
+        for (int i = 0; i < N; i++)
+            cin >> A[i];
+
+        int count = last(A, 0, N - 1, X) - first(A, 0, N - 1, X) + 1;
+        if (count == 1)
+            cout << -1 << "\n";
+        else
+            cout << count << "\n";
+    }
+    return 0;
+}
